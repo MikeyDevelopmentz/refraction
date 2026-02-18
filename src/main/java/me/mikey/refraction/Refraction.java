8ce125b11df3da9e24cbcaf0fe236bf61b57365b
unknown <thatundeadperson.io@gmail.com>
2026-02-18 13:42:19 -0800

    fixed retarded shit

--- src/main/java/me/mikey/refraction/Refraction.java
+++ src/main/java/me/mikey/refraction/Refraction.java
@@ -1,19 +1,21 @@
 package me.mikey.refraction;
 
+import me.mikey.refraction.check.impl.AutoAnchor;
+import me.mikey.refraction.check.impl.AutoTotem;
+import me.mikey.refraction.check.impl.FastCrystal;
 import me.mikey.refraction.command.AlertsCommand;
 import me.mikey.refraction.command.RefractionCommand;
 import me.mikey.refraction.command.RefractionTabCompleter;
 import me.mikey.refraction.config.CachedConfig;
 import me.mikey.refraction.manager.AlertManager;
-import me.mikey.refraction.manager.CheckManager;
 import me.mikey.refraction.manager.PlayerDataManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Refraction extends JavaPlugin {
    private static Refraction instance;
    private CachedConfig cachedConfig;
    private PlayerDataManager playerDataManager;
-   private CheckManager checkManager;
    private AlertManager alertManager;
 
    public void onEnable() {
@@ -23,7 +25,9 @@ public class Refraction extends JavaPlugin {
       cachedConfig.reload(getConfig(), getLogger());
       alertManager = new AlertManager();
       playerDataManager = new PlayerDataManager(this);
-      checkManager = new CheckManager(this);
+      Bukkit.getPluginManager().registerEvents(new FastCrystal(this), this);
+      Bukkit.getPluginManager().registerEvents(new AutoAnchor(this), this);
+      Bukkit.getPluginManager().registerEvents(new AutoTotem(this), this);
       getCommand("alerts").setExecutor(new AlertsCommand());
       getCommand("refraction").setExecutor(new RefractionCommand());
       getCommand("refraction").setTabCompleter(new RefractionTabCompleter());
@@ -42,10 +46,6 @@ public class Refraction extends JavaPlugin {
 
    public PlayerDataManager getPlayerDataManager() {
       return playerDataManager;
-   }
-
-   public CheckManager getCheckManager() {
-      return checkManager;
    }
 
    public AlertManager getAlertManager() {
